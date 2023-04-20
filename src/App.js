f79059c333c75e7c4f6bcd5bf92daa280cd10796
Harshit99312 <harshitsharma99312@gmail.com>
2023-04-20 15:06:07 +0530

    first textutils

---
+++ src/App.js
@@ -0,0 +1,83 @@
+
+import './App.css';
+import Alert from './components/Alert';
+//import About from './components/About';
+import About from './components/About';
+import Navbar from './components/Navbar';
+//import React from 'react';
+import TextForm from './components/TextForm';
+import React, {useState} from 'react';
+import {
+  BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+  function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+        setAlert(null);
+    }, 1500);
+  }
+  const removeBodyClasses = ()=>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success')
+  
+  }
+
+  const toggleMode = (cls)=>{
+    removeBodyClasses();
+    console.log(cls)
+    document.body.classList.add('bg-'+cls)
+    if(mode === 'light'){
+       setMode('dark');
+       document.body.style.backgroundColor = '#042743';
+       showAlert("Dark mode has been enabled", "success");
+       //document.title = 'TextUtils - Dark Mode';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+      //document.title = 'TextUtils - Light Mode';
+    }
+  }
+  return (
+    <>
+
+     {/*<Navbar title="TextUtils" aboutText="AboutTextUtils"/>*/}
+
+     {/* <Navbar/> */}
+     <Router>
+     <Navbar 
+     title="TextUtils" 
+     aboutText="About Us"
+     mode={mode}
+     toggleMode={toggleMode} 
+     />
+     <Alert alert={alert}/>
+     <div className="container my-3">
+       <Routes>
+          <Route exact path="/about" element={<About mode={mode} />} />
+          
+          <Route exact path="/" element ={
+             <TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove extra spaces" mode={mode}
+            />
+          }
+          />
+          </Routes>
+       </div>
+       </Router>
+     
+     </>
+  );
+}
+
+export default App;
---
+++ src/module2.mjs
@@ -0,0 +1,9 @@
+const a = "Harshit";
+const b = "Kumar";
+const c = "Aakash";
+const d = "Harry";
+
+export default b;
+export {a};
+export {c};
+export {d};
